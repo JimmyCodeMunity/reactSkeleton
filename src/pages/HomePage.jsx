8abffd9b39719d432bd465d31ef6b6eec5bb1c3b
Jimmy <jameswafula2002@gmail.com>
2024-07-17 15:25:34 +0300

    updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar'
 
 const HomePage = () => {
   return (
-    <div className="flex-1 w-full overflow-hidden bg-white">
+    <div className="flex-1 w-full overflow-hidden bg-primary">
       <div className="flex sm:px-16 px-6 justify-center items-center">
         <div className="w-full xl:max-w-[1280px]">
           <Navbar />
